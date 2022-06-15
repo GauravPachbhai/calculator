9761a4a81f29ae40e6ec9bb168540fe87631f677
GauravPachbhai <gauravpachbhai555@gmail.com>
2022-06-15 18:20:31 +0530

    code

--- cacl.js
+++ cacl.js
@@ -1,26 +1,61 @@
-let display= document.getElementById ('display');
-buttons = document.querySelectorAll('button');
-let screenValue = '';
-for(item of buttons){
-    item.addEventLestener('click' , (e)=>{
-        buttonText = e.taget.innerText;
-        console.log('Button text is',buttonText);
-        if(buttonText == 'X'){
-            buttonText = '*';
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
-        else if (buttonText == 'C'){
-            screenValue = "";
-            screen.value = screenValue;
-        }
-        else if (buttonText == '='){
-            screen.value = eval(screenValue);
-        }
-        else {
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
+var a = '';
+var b = '';
+var num = [];
+var ans;
 
-    })
+// All the numbers and operators input will be stored in an array "num" using function "sendNum()"
+function sendNum(digit){
+
+	num.push(digit);
+
+	if(num.length != 1){
+		a = '';
+		document.getElementById('screen').innerHTML = a;		// clearing the screen.
+	}
+
+
+	for(i=0; i<num.length ; i++){
+
+		a = a + num[i];				// concatenate the elements of the array "num" into a single string, which will be screened on the screen
+		
+	}
+
+document.getElementById('screen').innerHTML = a;	// screening the concatenated string
+
+	
+}
+
+// when the user presses "=", function "equalTo()" is called 
+function equalTo(){
+	document.getElementById('screen').innerHTML = '';
+
+	for(i=0; i<num.length ; i++){
+
+		b += num[i];						// concatenating the array "num" into a single string
+	}
+
+	ans = eval(b);	
+
+	document.getElementById('').innerHTML = ans;		// result screen
+
+	while(num.length > 0){
+    	num.pop();				// emptying the array "num"
+	}
+
+	num.push(ans.toString());
+
+	
+}
+
+
+// When user presses "AC", function "clearScr()" is called
+function clearScr(){
+	document.getElementById('screen').innerHTML = '';
+	
+	while(num.length > 0){
+    	num.pop();				// emptying the array "num"
+	}
+
+	a ='';	
+	b ='';
 }
